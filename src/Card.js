f2cfbc9dcecc7773fb3d8bac147ea89ca6204759
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-11-15 19:50:07 +0530

    moved images styles inside the constructure so that every card doesnt change all the time

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
 import './Card.css'
 export default class Card extends Component {
-  render() {
+  constructor(props) {
+    super(props)
     let angle = Math.random() * 90 - 45
     let xPos = Math.random() * 40 - 20
     let yPos = Math.random() * 40 - 20
-    let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`
-    console.log(transform)
+    this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`
+
+  }
+
+  render() {
+
     return (
       <div>
         <img
-          style={{ transform: transform }}
+          style={{ transform: this._transform }}
           className='card'
           src={this.props.image}
           alt={this.props.name}
